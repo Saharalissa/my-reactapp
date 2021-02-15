423f3368a9143a67d994dda027b56bb3e0053bdf
Saharalissa <sahar.alissa@gmail.com>
2021-02-15 13:13:53 +0200

    adding home route && adding lang button

--- my-reactapp/src/App.js
+++ my-reactapp/src/App.js
@@ -2,14 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 import React, {Component} from 'react';
 import ReactDom from 'react-dom';
-import { BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
-import Languages from './languages/Languages'
+import { BrowserRouter as Router, Switch, Route, Link, Redirect} from "react-router-dom";
+import Languages from './components/languages/Languages';
+import Home from './components/Home'
 
 function App() {
   return (
     <div className="App">
       <Router className="container">
-        <Route exact path="/:language" component={Languages} key={"/:language"} />
+        <Route exact path="/"><Redirect to="/home" /></Route>
+        <Route path="/:language" component={Languages} key={"/:language"} />
       </Router>
     </div>
   );
---
+++ my-reactapp/src/components/languages/Languages.js
@@ -0,0 +1,96 @@
+import React, {Component} from 'react';
+import ReactDom from 'react-dom';
+import LocalizedStrings from 'react-localization';
+import written from '../translations/index';
+
+var english =require("../translations/en.json");
+var arabic =require("../translations/ar.json");
+var string = new LocalizedStrings({en: english, ar: arabic});
+
+//functional component
+
+function Languages (props) {
+console.log(props)
+
+var local = props.match.params.language;
+let language = local ? local : "en";
+var direction = language === "ar" ? "rtl" : "ltr";
+string.setLanguage(language);
+
+// function handleLanguageChange(e) {
+//     console.log(e.target.value)
+//     language = e.target.value;
+//     string.setLanguage(language);
+//   }
+
+        return(            
+             <div>
+           {
+               (language === "ar")?(
+               <button><a  href="/en">English</a></button>
+               ) : (
+               <button><a  href="/ar">Arabic</a></button>
+               )
+           }
+           <div>{string.header}</div>
+           
+         </div>
+        )
+
+}
+
+export default Languages;
+
+//////////////////////
+
+//class component  
+
+//   class Languages extends Component {
+//     constructor(props) {
+//       super(props);
+//       this.state = {
+//         language: props.match.params.language
+//       }
+//       console.log(this.state.language)
+//       this.handleLanguageChange = this.handleLanguageChange.bind(this);
+//     }
+
+//     // This function is for the drop down list
+//     handleLanguageChange(e) {
+//       e.preventDefault();
+//       e.persist();
+//       console.log(this.state.language)
+//       let lang = e.target.value;
+   
+//       this.setState(prevState => ({
+//         language: lang
+//       }))
+//       console.log(this.state.language)
+     
+//       window.location.href=`/${e.target.value}`
+//       string.setLanguage(this.state.language);
+//     }
+  
+//     render() {
+//       string.setLanguage(this.state.language);
+//       return (
+//         <div>
+//           {
+//               (this.state.language === "ar")?(
+//               <button><a  href="/en">English</a></button>
+//               ) : (
+//               <button><a  href="/ar">Arabic</a></button>
+//               )
+//           }
+//           {/* Change Language: <select onChange={this.handleLanguageChange}>
+//             <option value="en">English</option>
+//             <option value="ar">Arabic</option>
+//           </select>
+//           <br /><br /> */}
+//           {string.header}
+//         </div>
+//       )
+//     }
+//   }
+
+// export default Languages;
