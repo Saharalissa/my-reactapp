b9c0e8601fc0cd17434c34d9fba27e3e4d305878
Saharalissa <sahar.alissa@gmail.com>
2021-02-24 15:29:08 +0200

    caseType

--- my-reactapp/src/components/card.js
+++ my-reactapp/src/components/card.js
@@ -4,24 +4,26 @@ import Form from './form';
 function Card ({data, cardIndex}) {
     const [status, setStatus] = useState(false);
     const [severity, setSeverity] = useState("");
+    // title from Card component = active from Quality component
     const [title, setTitle] = useState("");
+    const [type, setType] = useState("");
+
     return (
         <div>
             {data[cardIndex].map(item => (
             <div className="card">
-                  {console.log(item.title)}
-                <div onClick={() => {setStatus(true); setSeverity("Mild"); setTitle(item.title)}} key = {0}>{item.Mild}</div>
-                <div onClick={() => {setStatus(true); setSeverity("Moderate"); setTitle(item.title)}}  key = {1}>{item.Moderate}</div>
-                <div onClick={() => {setStatus(true); setSeverity("Severe"); setTitle(item.title)}}  key = {2}>{item.Severe}</div>
+                {console.log(item.title)}
+                <div onClick={() => {setStatus(true); setSeverity("Mild"); setTitle(item.title); setType("standard")}} key = {0}>{item.Mild}</div>
+                <div onClick={() => {setStatus(true); setSeverity("Moderate"); setTitle(item.title); setType("standard")}}  key = {1}>{item.Moderate}</div>
+                <div onClick={() => {setStatus(true); setSeverity("Severe"); setTitle(item.title); setType("plus")}}  key = {2}>{item.Severe}</div>
                 {console.log(severity)}
            </div>
             ))}
            <div> 
                 {/* {status === true && <Form/>} */}
                 {/* {status === false && null} */}
-                {status === true && <button onClick={() => {console.log(severity); console.log(title) }}>Get results</button>}
+                {status === true && <button onClick={() => {console.log("malocclusionType:",title); console.log("caseSeverity:",severity); console.log("caseType:", type ); }}>Get results</button>}
            </div>
-
         </div>
     )
 }
