560831581fbd74f089b97923ecc7f55a11a13751
Saharalissa <sahar.alissa@gmail.com>
2021-03-15 17:00:31 +0200

    appointment page (clinic select & date select)

--- my-reactapp/src/components/appointment.js
+++ my-reactapp/src/components/appointment.js
@@ -1,12 +1,163 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import LocalizedStrings from "react-localization";
+import Select,  { components } from 'react-select';
+import {Field, Formik, useFormik} from 'formik';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+import Pure_jo from '../images/Pure_jo.jpg';
+
+var english =require("./translations/en_clinic.json");
+var arabic =require("./translations/ar_clinic.json");
+var string = new LocalizedStrings({en: english, ar: arabic});
+
+const originalPath = window.location.pathname;
+var pathElements = originalPath.split("/"); //[0]=> / [1]=> en or ar
+const originalLocalePath = pathElements[1];
+let language = originalLocalePath;
+
+var direction = language === "ar" ? "rtl" : "ltr";
+string.setLanguage(language);
+
+const countries = {
+  en: [
+    { value: "jo", label: "Jordan" },
+    { value: "sa", label: "Saudi Arabia" },
+
+    { value: "kw", label: "Kuwait" },
+    { value: "qa", label: "Qatar" },
+    { value: "others", label: "Other Countries" },
+
+  ],
+  ar: [
+    { value: "jo", label: "الاردن" },
+    { value: "sa", label: "السعودية" },
+
+    { value: "kw", label: "الكويت" },
+    { value: "qa", label: "قطر" },
+    { value: "others", label: "دول اخرى" },
 
 
-function Appointment() {
+  ],
+};
+
+function Appointment(props) {
+// const [country, setCountry] = useState(props.country)
+// const [city, setCity] = useState(props.city)
+const country = props.country
+const city = props.city
+// const [clinics, setClinics] = useState(string[country]?.city[city].localizedClincsArray)
+const clinics = string[country]?.city[city].localizedClincsArray
+const [clinic, setClinic] = useState("")
+const [selectedDate, setSelectedDate] = useState(null)
+const [image, setImage] = useState(null)
+
+console.log(props.country)
+console.log(props.city)
+console.log(country)
+console.log(city)
+
+function handleChange(e) {
+  // setClinics(string[country]?.city[city].localizedClincsArray)
+  setClinic(e.target.value);
+  console.log(e.target.value)
+}
+  console.log(clinic)
+  console.log(clinics)
+function handleSubmit(e) {
+  e.preventDefault();
+  console.log(clinic, selectedDate);
+}
+
     return(
         <div>
-            appointment page
+            <div className="clinic-form-image-grid">
+                <div></div>
+                <div></div>{console.log(string)}
+            </div>
+            {/* {props.country}<br/>{props.city}<br/> */}
+            {country}<br/>{city}<br/>
+            <form  onSubmit={handleSubmit}>
+              <label>
+                {string.choose_a_clinic}<br/>
+                <select value={clinic} placeholder="" onChange={handleChange}>
+                  <option>{}</option>
+                  {/* <option >{string[country]?.city[city].localizedClincsArray}</option>    */}
+                  {/* {countries[language].map((item, index) => ( */} 
+                  {string[country]?.city[city].localizedClincsArray.map((item, index) => (
+                  <option key = {item.index} value={item.value}>{item}</option>       
+               ))}
+                 
+            </select><br/>
+                <label>
+                  {string.choose_the_times_that_suits_you}<br/>
+                  <DatePicker selected={selectedDate} onChange={date => setSelectedDate(date)}
+                   dateFormat = 'E-dd/MM/yy'
+                   minDate = {new Date()}
+                  // filterDate = {date => date.getDay() != 6 && date.getDay() != 0}
+                  filterDate = {date => date.getDay() != 5}
+                  />
+                </label><br/>
+                </label><br/>
+              <input type="submit" value="Submit" />
+            </form>
+            {console.log(props.country)}
+            {console.log(props.city)}
+            {/* {string.ae.clincNameOption} */}
+            {/* {string[country]?.clincNameOption} */}
+            {/* {string[country]? string[country].clincNameOption:""} */ }
+            {/* same meaning as the above */}
+            {/* {string[country]?.city[city].clincNameOption} */}
         </div>
     )
 }
-
 export default Appointment;
+
+
+
+// function Appointment(props) {
+// const [country, setCountry] = useState(props.country)
+// const [city, setCity] = useState(props.city)
+// console.log(props.country)
+// console.log(props.city)
+
+// const formik = useFormik({
+//     initialValues:{
+//         Country:  "",
+//         City: "",
+//         Email:'',
+//         preferedLanguage: language
+//     },    
+//     validate,
+//     onSubmit: values => {
+//         console.log(JSON.stringify(values, null, 2))
+//     }
+// })
+//     return(
+//         <div>
+//             <div className="clinic-form-image-grid">
+//                 <div><CustomSelect
+//                        language = {language}
+//                        options={countries[language]}
+//                        value={formik.values.Country}
+//                        onChange={value=>{
+//                        formik.setFieldValue('Country', value.value)
+//                        formik.setFieldValue('City' ,cities2[language][value.value][0])
+//                        formik.setFieldValue('countryCode', formik.values.countryCode) //forEach
+//                       // formik.setFieldValue('countryCode',countryCodeFromCountry(value.value))
+//                       //  this is to hide the error message
+//                        formik.setFieldTouched('Country' ,false )
+//                       }}
+//                 /></div>
+//                 <div></div>{console.log(string)}
+//             </div>
+//             appointment page<br/>
+//             {props.country}<br/>
+//             {console.log(props.country)}
+//             {console.log(props.city)}
+//             {props.city}
+//         </div>
+//     )
+// }
+// export default Appointment;
+
+
--- my-reactapp/src/components/emailConfirmation.js
+++ my-reactapp/src/components/emailConfirmation.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, {useEffect, useState} from 'react';
 import Axios from 'axios';
 import queryString from "query-string";
 import LocalizedStrings from "react-localization";
@@ -16,14 +16,85 @@ let language = originalLocalePath;
 var direction = language === "ar" ? "rtl" : "ltr";
 string.setLanguage(language);
 
+// const includes = require("lodash/includes");
+// const standardizedCountryCode = (incoming) => {
+//     if (
+//       includes(
+//         ["uae", "ae", "united arab emirates", "arab emirtaes"],
+//         incoming.toLowerCase()
+//       )
+//     ) {
+//       return "ae";
+//     }
+ 
+//     if (includes(["sa", "saudi", "saudi arabia"], incoming.toLowerCase())) {
+//       return "sa";
+//     }
+ 
+//     if (includes(["jo", "jordan"], incoming.toLowerCase())) {
+//       return "jo";
+//     }
+ 
+//     if (includes(["lb", "lebanon"], incoming.toLowerCase())) {
+//       return "lb";
+//     }
+ 
+//     if (includes(["iq", "iraq"], incoming.toLowerCase())) {
+//       return "iq";
+//     }
+ 
+//     if (includes(["qa", "qatar"], incoming.toLowerCase())) {
+//       return "qa";
+//     }
+ 
+//     if (includes(["kw", "kuwait"], incoming.toLowerCase())) {
+//       return "kw";
+//     }
+ 
+//     return "n/a";
+//   };
+
+const standardizedCountryCode = (incoming) => {
+    if (incoming === 'United Arab Emirates')
+     { return "ae";}
+ 
+    if (incoming === 'Saudi Arabia') {
+      return "sa";
+    }
+ 
+    if (incoming === 'Jordan') {
+      return "jo";
+    }
+ 
+    if (incoming === 'Lebanon') {
+      return "lb";
+    }
+ 
+    if (incoming === 'Iraq') {
+      return "iq";
+    }
+ 
+    if (incoming === 'Qatar') {
+      return "qa";
+    }
+ 
+    if (incoming === 'kuwait') {
+      return "kw";
+    }
+ 
+    return "n/a";
+  };
+  let standardCountryCode;
 
 function Confirmation() { 
     const [exists,setExists] =useState(false)
     const [email,setEmail] =useState("")
+    const [country,setCountry] =useState("")
+    const [city,setCity] =useState("")
 
     //There are two axios functions for the same link, one for the click and one of the page when is
     //loaded to check the queryparams
-    function handleClick(e) {
+function handleClick(e) {
     e.preventDefault();
     console.log('The button was clicked.');
     Axios.post(` https://assessment.12staging.com/capture/funnel3/validateEmail`, {email})
@@ -31,10 +102,18 @@ function Confirmation() {
     console.log(res);
     console.log(res.data);
     setExists(res.data.exists);
+    setCountry(res.data.country);
+    standardCountryCode= standardizedCountryCode(res.data.country);
+    setCity(res.data.city);
     if(res.data.exists === true) {
-        window.location=`/${language}/appointment?email=${email}`//fixed
+        window.location=`/${language}/confirmation?email=${email}`//fixed, here I had to use /conformation to get the props country and city
     } else {
-        window.location=`/${language}`         
+        window.location=`/${language}`
+        // if (language = 'en'){
+        //    window.location=`/en` 
+        // }else {
+        //    window.location=`/ar` 
+        // }   
     }
     console.log(res.data.exists);
     console.log(email);
@@ -42,8 +121,8 @@ function Confirmation() {
     console.log(error);
          })
       }
-
-    let recivedData = queryString.parse(window.location.search);
+    
+    useEffect(()=>{let recivedData = queryString.parse(window.location.search);
     console.log(recivedData)
     Axios.post(` https://assessment.12staging.com/capture/funnel3/validateEmail`, recivedData)
     .then(res => {
@@ -51,16 +130,20 @@ function Confirmation() {
     console.log(res.data);
     setExists(res.data.exists)
     console.log(res.data.exists);
+    setCountry(res.data.country);
+    standardCountryCode= standardizedCountryCode(res.data.country);
+    setCity(res.data.city);
     // let postedEmail=
     // window.location.search ? `${window.location.search}&email=${body.email}`: `?email=${body.email}`;
     // window.location=`/${language}/confirmation${postedEmail}`
     // console.log(postedEmail)
     }).catch(error => {
     console.log(error);
-         })
+         })}, [])
+    
     return(
         <div>
-            {exists? <Appointment/> :  
+            {exists? <Appointment country={standardCountryCode} city={city}/> :  
             <div style={{direction: direction}}>
             <p>{string.email}</p>
             <input name="Email" placeholder={string.holder} className="form-control" onChange={e=>{setEmail(e.target.value)}}/>
@@ -71,61 +154,3 @@ function Confirmation() {
 }
 
 export default Confirmation;
-
-
-// import React from 'react';
-// import Axios from 'axios';
-// import queryString from "query-string";
-// import LocalizedStrings from "react-localization";
-// import { render } from 'react-dom';
-
-// const originalPath = window.location.pathname;
-// var pathElements = originalPath.split("/"); //[0]=> / [1]=> en or ar
-// const originalLocalePath = pathElements[1];
-// let language = originalLocalePath;
-
-
-// class Confirmation extends React.Component { 
-//     constructor(props) {
-//         super(props);
-//         this.state = {value: ''};
-    
-//         this.handleChange = this.handleChange.bind(this);
-//         this.handleSubmit = this.handleSubmit.bind(this);
-//       }
-//       handleChange(event) {
-//         this.setState({value: event.target.value});
-//         console.log(event.target.value)
-//       }  
-
-//     handleSubmit(event) {
-//         //  let recivedData = queryString.parse(window.location.search)
-//         alert('A name was submitted: ' + this.state.value);
-//         event.preventDefault();
-//         let recivedData = this.state.value
-//     console.log(recivedData)
-//     Axios.post(` https://assessment.12staging.com/capture/funnel3/validateEmail`, recivedData)
-//     .then(res => {
-//     console.log(res);
-//     console.log(res.data);
-//     console.log(res.data.exists);
-//     // let postedEmail=
-//     // window.location.search ? `${window.location.search}&email=${body.email}`: `?email=${body.email}`;
-//     // window.location=`/${language}/appointment-request${recivedData}`
-//     // console.log(postedEmail)
-//     }).catch(error => {
-//     console.log(error);
-//          })
-//     }
-//    render(){
-//     return(
-//         <div>
-//             <p>Enter your email to request an appointment</p>
-//             <input name="Email" placeholder="Email" className="form-control" onChange={this.handleChange}/>
-//             <button className="submit-form-button-1" type="submit" onSubmit={this.handleSubmit} >Confirm</button>
-//         </div>
-//     )
-// }
-// }
-
-// export default Confirmation;
